dc7a5856835ccc7e7b5fb2e3781eb3e52b80a20d
Kiki-tech-eng <kusorogloria@gmail.com>
2025-04-05 13:54:21 +0100

    Tailwind config

---
+++ interactive-card-details-form-main/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // This works recursively for all folders
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
